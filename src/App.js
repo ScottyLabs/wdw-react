d0455f600275bcf66dc0433deb63af8ed65493bb
David Ethan Hwang <hdavidethan@gmail.com>
2020-10-09 20:47:33 -0400

    Fixed time conversion

--- src/App.js
+++ src/App.js
@@ -30,9 +30,13 @@ function App()  {
     const minutes = date.getMinutes();
     let period = 'AM';
     
-    if (hour > 12) {
-      hour = hour % 12;
+    if (hour >= 12) {
+      hour = hour - 12;
       period = 'PM';
+    }
+
+    if (hour == 0) {
+      hour = 12;
     }
 
     let minuteString = '';
